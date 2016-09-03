f04792ae853d2db1a5aab2cd0b705dd4f3a46735
m <m@m.com>
2016-09-04 00:07:36 +0100

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
-var port = process.env.PORT || 3000;
+app.set('port', (process.env.PORT || 5000));
 
 app.use('/static', express.static(__dirname + '/src/client/public'));
 
@@ -10,4 +10,6 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/src/client/index.html'));
 });
 
-app.listen(port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
